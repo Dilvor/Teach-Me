a68ece76fc9d3fc0ee8f7da57f4562d6ac865367
Dilvor <Sig4232258Sig>
2022-02-04 17:26:24 +0300

    add addon for first home work

---
+++ src/main/java/HomeWork/AddonFirstWork.java
@@ -0,0 +1,9 @@
+package HomeWork;
+
+public class AddonFirstWork {
+    public static void main(String[] args) {
+        double angle = 45 * Math.PI/180;
+        double value = Math.sin(angle);
+        System.out.println("val="+value);
+    }
+}
