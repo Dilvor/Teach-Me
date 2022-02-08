b45a68655d585498b88111134fe9e45ded16bc1c
Dilvor <Sig4232258Sig>
2022-02-08 16:45:27 +0300

    added addon for second house work

---
+++ src/main/java/HomeWork/AddonSecondWork.java
@@ -0,0 +1,15 @@
+package HomeWork;
+
+public class AddonSecondWork {
+    public static int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Факториал числа 5 = " + getFactorial(5));
+    }
+}
