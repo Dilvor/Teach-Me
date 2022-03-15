7b8f0e636b59b3a2b948b77284d53e212684012c
Dilvor <Sig4232258Sig>
2022-03-15 16:11:32 +0300

    небольшой апдейт

--- src/main/java/homeWork/homeWork10/AutomobileRunner.java
+++ src/main/java/homeWork/homeWork10/AutomobileRunner.java
@@ -8,15 +8,21 @@ public class AutomobileRunner {
         Truck Truck1 = new Truck(666, 50, 9000, 2.0, 1.5);//Слишком тяжелый
         Truck Truck2 = new Truck(777, 50, 1000, 5.0, 1.5);//Слишком высокий
         Truck Truck3 = new Truck(888, 50, 1000, 2.0, 4.5);//Слишком широкий
+
+        int limitSpeed = 80;//превышение скорости
+        int violationSpeed = 100;//сильное превышение скорости
+        int maxWeight = 8000;//максимально допустимый вес(в КГ)
+        double maxWidth = 4.0;//максимально допустимая ширина(в метрах)
+        double maxHeight = 2.5;//максимально допустимая высота(в метрах)
         Automobile[] automobiles = new Automobile[]{Car1, Car2, Car3, Truck1, Truck2, Truck3};
         for (Automobile automobile : automobiles) {
-            if (81 <= automobile.speed) {
-                System.out.println("Автомобиль с номером: " + automobile.number + " атата,еще немного и хуже будет");
+            if (limitSpeed < automobile.speed) {
+                System.out.println("Автомобиль с номером: " + automobile.number + " атата");
             }
-            if (101 <= automobile.speed) {
-                System.out.println("Автомобиль с номером: " + automobile.number + " доигрался,задержан полицией");
+            if (violationSpeed < automobile.speed) {
+                System.out.println("Автомобиль с номером: " + automobile.number + " задержан полицией");
             }
-            if (8001 <= automobile.weight | 4.1 <= automobile.width | 2.6 <= automobile.height) {
+            if (maxWeight < automobile.weight | maxWidth < automobile.width | maxHeight < automobile.height) {
                 System.out.println("На автомобиль с номером: " + automobile.number +
                         " невозможно проехать КПП из за несоотвествия габаритов");
             }
